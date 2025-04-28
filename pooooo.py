70adcb7329f02bcff33d8cf594ed03e6f30d45f3
sarathxxx <145278261+sarathxxx@users.noreply.github.com>
2025-04-28 07:08:51 +0530

    My first code

---
+++ pooooo.py
@@ -0,0 +1,12 @@
+currect_password = "sarath@32"
+chance = 0
+while chance < 4:
+    input_password = input("Enter your password: ")
+    if input_password != currect_password:
+        chance += 1
+        print("Incorrect password, please try again.")
+    else:
+        print("Access granted!")
+    break
+else:
+    print("try again after 1 hour")
